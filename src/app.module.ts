a9d01fe065ad8de5d83c529aac2f891bf7779017
Gabriel <gabrielmussolini@hotmail.com>
2021-02-27 08:46:06 -0300

    fix missconfiguration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { User } from './models/User';
-import { UserController } from './user/user.controller';
-
+import { PlaceModule } from './place/place.module';
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.TYPEORM_HOST,
-      port: parseInt(process.env.TYPEORM_PORT),
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      entities: [User],
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([User]),
-  ],
-  controllers: [AppController, UserController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), PlaceModule],
 })
 export class AppModule {}
